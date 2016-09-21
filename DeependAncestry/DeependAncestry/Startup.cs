d6c5038865af0cbdc35527098eb2a868e8f6fe83
Joseph Eskander <joseph.eskander@onset.com.au>
2016-09-21 20:17:12 +1000

    Initial commit: project created

---
+++ DeependAncestry/DeependAncestry/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeependAncestry.Startup))]
+namespace DeependAncestry
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
