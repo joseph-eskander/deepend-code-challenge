9227bc31986edb01c598b644bca1858610af5fb6
Joseph Eskander <joseph.eskander@onset.com.au>
2016-09-24 18:41:42 +1000

    Add simple search page

--- DeependAncestry/DeependAncestry/App_Start/RouteConfig.cs
+++ DeependAncestry/DeependAncestry/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace DeependAncestry
@@ -16,7 +12,7 @@ namespace DeependAncestry
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Search", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ DeependAncestry/DeependAncestry/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace DeependAncestry.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Advanced()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
